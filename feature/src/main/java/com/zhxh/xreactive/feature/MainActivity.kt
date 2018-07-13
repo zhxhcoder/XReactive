899a6d7df2366577af97035076d928206c642d32
zhxh <zhxhcoder@gmail.com>
2018-07-13 14:35:46 +0800

    OptionsMenu

--- feature/src/main/java/com/zhxh/xreactive/feature/MainActivity.kt
+++ feature/src/main/java/com/zhxh/xreactive/feature/MainActivity.kt
@@ -48,27 +48,6 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        val id = item.itemId
-
-        if (id == R.id.action_settings) {
-            return true
-        }
-
-        return super.onOptionsItemSelected(item)
-    }
-
-
     /**
      * A [FragmentPagerAdapter] that returns a fragment corresponding to
      * one of the sections/tabs/pages.
